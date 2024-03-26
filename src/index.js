a762f7dc6fbacfb90139c97a06e8023ebbf18c99
FranciscoSMP <fmoralesp2001@gmail.com>
2024-03-26 00:23:46 -0600

    26/03/2024

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-// Middlewares
+// Configuraciones.
+
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares.
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Rutas.
 
 // Iniciando el Servidor.
 
-app.listen(3000, () => {
-    console.log(`Servidor en el puerto ${3000}`);
+app.listen(app.get('port'), () => {
+    console.log(`Servidor en el puerto ${app.get('port')}`);
 });
